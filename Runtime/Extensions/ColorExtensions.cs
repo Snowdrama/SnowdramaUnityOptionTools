f925de6eb9fe5c9dd25c4599783d8fabd9e6ff05
John 'Snow' Close <SnowdramaGames@gmail.com>
2022-10-11 01:12:49 -0400

    Redid some stuff around the color extension

--- Runtime/Extensions/ColorExtensions.cs
+++ Runtime/Extensions/ColorExtensions.cs
@@ -3,25 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Color
+public static class ColorExtension
 {
-    public static UnityEngine.Color ParseHexString(string hexString, UnityEngine.Color fallback = default)
+    public static Color ParseStringAsColor(this string hexString, Color fallback = default)
     {
-        UnityEngine.Color color = fallback;
+        Color color = fallback;
         ColorUtility.TryParseHtmlString(hexString, out color);
         return color;
     }
 
-    public static UnityEngine.Color GetColorFromRainbow(float t, float of = 1, UnityEngine.Gradient gradient = null)
+    public static Color GetColorFromRainbow(float t, float of = 1, Gradient gradient = null)
     {
-        UnityEngine.Gradient g;
+        Gradient g;
         if (gradient == null)
         {
             g = gradient;
         }
         else
         {
-            g = new UnityEngine.Gradient();
+            g = new Gradient();
             GradientColorKey[] colorKeys = new GradientColorKey[8];
             GradientAlphaKey[] alphaKeys = new GradientAlphaKey[2];
             alphaKeys[0].alpha = 1;
@@ -29,14 +29,14 @@ public static class Color
             alphaKeys[0].time = 0;
             alphaKeys[0].time = 1;
 
-            colorKeys[0].color = UnityEngine.Color.red;
-            colorKeys[1].color = UnityEngine.Color.yellow;
-            colorKeys[2].color = UnityEngine.Color.green;
-            colorKeys[3].color = UnityEngine.Color.cyan;
-            colorKeys[4].color = UnityEngine.Color.blue;
-            colorKeys[5].color = UnityEngine.Color.magenta;
-            colorKeys[6].color = UnityEngine.Color.white;
-            colorKeys[7].color = UnityEngine.Color.black;
+            colorKeys[0].color = Color.red;
+            colorKeys[1].color = Color.yellow;
+            colorKeys[2].color = Color.green;
+            colorKeys[3].color = Color.cyan;
+            colorKeys[4].color = Color.blue;
+            colorKeys[5].color = Color.magenta;
+            colorKeys[6].color = Color.white;
+            colorKeys[7].color = Color.black;
 
             colorKeys[0].time = 0.0f;
             colorKeys[1].time = 0.14f * 1;
